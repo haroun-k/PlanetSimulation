2992fd16d00e24308670d2d770661d714db79c9e
Krifa Haroun <haroun.krifa@etu.sorbonne-universite.fr>
2023-03-14 10:29:55 +0000

    transformation du script en class instanciable

--- Projet/Scripts/AgentScriptTest.gd
+++ Projet/Scripts/AgentScriptTest.gd
@@ -1,5 +1,4 @@
-
-extends SpringArm3D
+class_name Agent extends SpringArm3D
 @export var current_direction : Vector3
 @export var current_position : Vector3
 
@@ -18,47 +17,6 @@ func get_closest_center(pos:Vector3, points:Array):
 			closest_distance = pos.distance_to(point)
 	return closest_point
 
-func action():
-	var tab = world.centersDictionary.keys()
-	var pooo = get_closest_center($MeshInstance3D.global_position,tab)
-	
-#
-#func from_spherical_to_cartesian(vec : Vector3):
-#	var r=vec.x*sin(vec.z)
-#	var x = sin(vec.y)*r
-#	var y = vec.x*sin(vec.z)
-#	var z = cos(vec.y)*r
-#	return Vector3(z,y,x)
-#
-#func from_cartesian_to_spherical(pos:Vector3):
-#	var rho = sqrt(pos.z**2+(sqrt(pos.z**2+pos.x**2)))
-#	var theta = fposmod( atan2(pos.x,pos.z)+2*PI,2*PI)
-#	var phi = fposmod( atan2(pos.y,pos.z)+2*PI,2*PI)
-#	return Vector3(5,theta,phi)
-
-func from_spherical_to_cartesian(vec:Vector3):
-	var r=vec.x
-	var theta=vec.y
-	var phi=vec.z
-	return Vector3(r*sin(theta)*cos(phi),r*sin(theta)*sin(phi),r*cos(theta))
-func from_cartesian_to_spherical(pos:Vector3):
-	var r = pos.length()
-	var theta = acos(pos.z/r)
-	var phi = atan2(pos.y,pos.x)
-	return Vector3(r,theta,phi)
-
-func rotate_around_x_axis(pos:Vector3, angle:float):
-	var x = pos.x
-	var y = pos.y*cos(angle)-pos.z*sin(angle)
-	var z = pos.y*sin(angle)+pos.z*cos(angle)
-	look_at(global_position*2)
-	return Vector3(x,y,z)
-func rotate_around_y_axis(pos:Vector3, angle:float):
-	var x = pos.x*cos(angle)+pos.z*sin(angle)
-	var y = pos.y
-	var z = -pos.x*sin(angle)+pos.z*cos(angle)
-	look_at(global_position*2)
-	return Vector3(x,y,z)
 func rotate_towards_point(destination:Vector3):
 	var axis = global_position.cross(destination)
 	var angle = acos(global_position.dot(destination)/(global_position.length()*destination.length()))
@@ -72,40 +30,57 @@ func rotate_around_axis(pos:Vector3, axis:Vector3, angle:float):
 	look_at(global_position*2)
 	return Vector3(x,y,z)
 
-
-func movement():
-	var tran3D = Transform3D(Vector3.RIGHT,Vector3.UP,Vector3.BACK,Vector3.ZERO)
-	var sphereCoo=from_cartesian_to_spherical(global_position)
-	if Input.is_action_pressed("up") :
-		rotate_towards_point(Vector3(0,2,0))
-	if Input.is_action_pressed("right") :
-		global_position=rotate_around_y_axis(global_position,0.05)
-		
-		
-	if Input.is_action_pressed("down") :
-		global_position=rotate_around_x_axis(global_position,-0.05)
-	if Input.is_action_pressed("left") :
-		global_position=rotate_around_y_axis(global_position,-0.05)
-
 func update_position():
-	current_position=get_closest_center($MeshInstance3D.global_position,world.centersDictionary.keys())
+	current_position=get_closest_center(get_child(0).global_position,world.centersDictionary.keys())
+	
 func take_random_direction():
 	current_direction=world.centersNeighboursDictionary[current_position][randi_range(0,2)]
-	
+
+func go_to_random_position():
+	rotate_towards_point(Vector3(randf(),randf(),randf()))
 func auto_move():
 	if current_direction == current_position:
 		update_position()
 		take_random_direction()
 	rotate_towards_point(current_direction)
-	$MeshInstance3D/directionMesh.global_position=current_direction
+	get_child(0).get_child(0).global_position=global_position
 	update_position()
 	
+	
+	
+	
+	
+	
+	
 func _process(delta):
-	movement()
 	auto_move()
 	
 func _ready():
+	var arr_mesh = ArrayMesh.new()
+	
+	var cube = BoxMesh.new()
+	
+	var agentMesh = MeshInstance3D.new()
+	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, cube.surface_get_arrays(0) )
+	agentMesh.mesh=arr_mesh
+	agentMesh.set_scale(Vector3(0.1, 0.1, 0.1))
+	
+	
+	var directionMesh = MeshInstance3D.new()
+	arr_mesh.add_surface_from_arrays(Mesh.PRIMITIVE_TRIANGLES, cube.surface_get_arrays(0) )
+	directionMesh.mesh=arr_mesh
+	directionMesh.set_scale(Vector3(0.05, 0.05, 0.05))
+	
+	margin=0.05
+	spring_length=200
+	print("margin = ", margin, ", spring_length = ", spring_length)
+	add_child(agentMesh)
+	print(get_child_count())
+	print(get_child(0))
+	get_child(0).add_child(directionMesh)
+	global_position=Vector3(0,0,2)
+#	go_to_random_position()
+	
 	update_position()
 	take_random_direction()
-#	rotate_towards_point(current_direction)
 	
