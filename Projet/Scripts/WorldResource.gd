fa3ae69d276e4e9504b78fda8371135967bd8efc
Krifa Haroun <haroun.krifa@etu.sorbonne-universite.fr>
2023-03-17 09:48:29 +0000

    resource monde

---
+++ Projet/Scripts/WorldResource.gd
@@ -0,0 +1,12 @@
+class_name WorldResource extends Resource
+
+@export var resolution :int
+
+#@export var pointNeighbours :={}
+#@export var equivalentPoints :={}
+
+
+@export var centersDictionary := {}
+var centersArray = centersDictionary.keys()
+@export var centersNeighboursDictionary := {}
+@export var tilesData : Array[TileResource]
