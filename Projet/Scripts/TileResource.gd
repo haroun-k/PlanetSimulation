fa75bff5b6d3a01bb0146eb3ec0b4d67a9e6a69f
Krifa Haroun <haroun.krifa@etu.sorbonne-universite.fr>
2023-03-18 11:29:17 +0000

    fonctions mises en place

--- Projet/Scripts/TileResource.gd
+++ Projet/Scripts/TileResource.gd
@@ -1,8 +1,42 @@
 class_name TileResource extends Resource
 
-@export_enum("WATER","GRASS","MUD") var TERRAIN_TYPE : int
+@export var tile_position : Vector3
+enum TERRAIN_TYPE {WATER,GRASS,MUD,FIRE}
 @export var terrainType : int
-@export_color_no_alpha var tileColor : Color
-@export var voisins : Array[TileResource]
-func update_tile(voisin1 : TileResource, voisin2 : TileResource, voisin3 : TileResource):
-	pass
+@export_color_no_alpha var tileColor : Color :
+	get : return get_color()
+@export var neighbours : Array[TileResource]
+
+
+func get_color():
+	match terrainType :
+		TERRAIN_TYPE.GRASS :
+			return Color.SPRING_GREEN
+		TERRAIN_TYPE.WATER :
+			return Color.DEEP_SKY_BLUE
+		TERRAIN_TYPE.MUD :
+			return Color.BURLYWOOD
+		TERRAIN_TYPE.FIRE :
+			return Color.DARK_RED
+		_ : return Color.DARK_SLATE_GRAY
+
+
+func init_tile():
+	terrainType = TERRAIN_TYPE.GRASS
+	if(randf()<0.2) : terrainType = TERRAIN_TYPE.WATER 
+	if(randf()<0.1) : terrainType = TERRAIN_TYPE.FIRE 
+
+func update_tile():
+	if terrainType==TERRAIN_TYPE.GRASS and ( neighbours[0].terrainType == TERRAIN_TYPE.FIRE or neighbours[1].terrainType == TERRAIN_TYPE.FIRE or neighbours[2].terrainType == TERRAIN_TYPE.FIRE):
+		terrainType=TERRAIN_TYPE.FIRE
+	elif terrainType==TERRAIN_TYPE.GRASS and (randf()<0.0005) : terrainType = TERRAIN_TYPE.FIRE 
+		
+	elif terrainType==TERRAIN_TYPE.MUD and ( neighbours[0].terrainType == TERRAIN_TYPE.GRASS or neighbours[1].terrainType == TERRAIN_TYPE.GRASS or neighbours[2].terrainType == TERRAIN_TYPE.GRASS):
+		terrainType=TERRAIN_TYPE.GRASS
+	elif terrainType==TERRAIN_TYPE.MUD and (randf()<0.1) : terrainType = TERRAIN_TYPE.GRASS 
+		
+	elif terrainType==TERRAIN_TYPE.FIRE :
+		for i in neighbours :
+			if(i.terrainType==TERRAIN_TYPE.GRASS): i.terrainType=TERRAIN_TYPE.FIRE
+			terrainType=TERRAIN_TYPE.MUD
+	
