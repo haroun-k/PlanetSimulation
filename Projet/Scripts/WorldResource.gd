2a5e057416992775ffb03fc802563b2d4d36ec63
Baumann Pierre <pierre.baumann@etu.sorbonne-universite.fr>
2023-04-08 18:10:22 +0000

    Updated WorldResource

--- Projet/Scripts/WorldResource.gd
+++ Projet/Scripts/WorldResource.gd
@@ -30,16 +30,15 @@ class SetFile :
 @export var maxAmountOfTrees : int
 @export var amountOfTrees : int 
 @export var points : Array = [null]
-@export var pointDictionnary : Dictionary
 
 
 @export var centersDictionary : Dictionary
 @export var centersNeighboursDictionary := {}
 
-
 @export var tilesData : Array[TileResource]
-@export var updateTilesArray : Array[TileResource]
 @export var entities : Array[Entity]
+
+var atmosphere
 
 @export var colors : PackedColorArray : 
 	get : 
@@ -53,10 +52,11 @@ func get_edge_index(pt : Vector3) :
 func get_point_index_ordered(center : Vector3) : 
 	return centersDictionary.keys().find(center)
 
-func init_world():
+func init_world(atmosphere):
+	
+	self.atmosphere = atmosphere
 	var tiles_a_traiter = SetFile.new()
 	tilesData.clear()
-	entities.resize(points.size())
 	var ki = 0
 	var clefs = centersDictionary.keys()
 	for k in clefs :
@@ -82,17 +82,13 @@ func init_world():
 
 	while not tiles_a_traiter.est_vide() :
 		var tile = tiles_a_traiter.defiler()
-		if tile.collapse_tile():
+		if tile.collapse_tile(atmosphere.temperature):
 			tiles_a_traiter.enfiler_unchecked(tile)
 	for t in tilesData:
 		if t.isUndefined():
-			t.collapse_tile()
-	for cent in centersDictionary:
-		for pt in centersDictionary[cent] :
-			pointDictionnary[pt]= [] if not pointDictionnary.keys().has(pt) else pointDictionnary[pt] + [cent] 
-	updateTilesArray=tilesData.duplicate(true)
-	updateTilesArray.shuffle()
-	generate_astar()
+			t.collapse_tile(atmosphere.temperature)
+
+	init_asta()
 	
 func get_entities_on_tile(position: Vector3):
 	var arr := []
@@ -112,15 +108,17 @@ func spawn_entities(world : Node3D):
 	for td in tilesData :
 		if td.terrainType == TileResource.TERRAIN_TYPE.GRASS :
 			var entityPos = centersDictionary[td.tile_position].pick_random()
-			if randf()<(1-(amountOfTrees/maxAmountOfTrees))/10000. and entities[get_edge_index(entityPos)]== null:
+			if randf()<(1-(amountOfTrees/maxAmountOfTrees) )/10000. and entities[get_edge_index(entityPos)]== null:
 				amountOfTrees+=1
 				init_entity(entityPos)
 				world.add_child(entities[get_edge_index(entityPos)])
+				
+		entities.resize(points.size())
 
 func init_entity(pos : Vector3):
 	entities[get_edge_index(pos)]=Entity.new(pos)
 	
-func generate_astar():
+func init_asta():
 	var myastar = AStar3D.new()
 	for i in centersNeighboursDictionary.keys():
 		var idi = get_point_index_ordered(i)
@@ -132,30 +130,21 @@ func generate_astar():
 			if tilesData[idj].terrainType==TileResource.TERRAIN_TYPE.WATER : myastar.set_point_disabled(idj)
 		if tilesData[idi].terrainType==TileResource.TERRAIN_TYPE.WATER : myastar.set_point_disabled(idi)
 	myAstar=myastar
-
-func update_world_resource():
-	for i in updateTilesArray :
-		var previousType=i.terrainType
-		i.update_tile(waterHeight)
-		if i.terrainType==TileResource.TERRAIN_TYPE.WATER and previousType!=TileResource.TERRAIN_TYPE.WATER :
-			myAstar.set_point_disabled(get_point_index_ordered(i.tile_position))
-		elif i.terrainType!=TileResource.TERRAIN_TYPE.WATER and previousType==TileResource.TERRAIN_TYPE.WATER :
-			myAstar.set_point_disabled(get_point_index_ordered(i.tile_position),false)
-	for entity in entities:
-		if entity !=null :
-			var toDelete = true
-			for surroundingCenters in pointDictionnary[entity.entityResource.position] :	
-				if tilesData[get_point_index_ordered(surroundingCenters)].terrainType!=TileResource.TERRAIN_TYPE.WATER :
-					toDelete=false
-			if toDelete : 
-				entity.queue_free()
-				amountOfTrees-=1
-		
-
+				
 func get_world():
-
+	
+#	pass
+	for i in range(15) : 
+		tilesData.pick_random().collapse_tile(atmosphere.temperature)
 	var res = []
+#	res.resize(tilesData.size()*3)
 	for i in tilesData : 
+		i.update_tile(waterHeight, atmosphere.temperature)
+#		var colr=i.get_color()
+#		var index = get_point_index_ordered(i.tile_position)
+#		res[index]= colr
+#		res[index+1]= colr
+#		res[index+2]= colr
 		res.push_back(i.get_color())
 		res.push_back(i.get_color())
 		res.push_back(i.get_color())
