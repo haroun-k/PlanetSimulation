56709244a99e01eac96add33b916efe29c3ac6f0
Krifa Haroun <haroun.krifa@etu.sorbonne-universite.fr>
2023-04-12 19:39:49 +0000

    Suppression fichier inutile crée par erreur

--- Scripts/rui/tempSliderLabel.gd
+++
@@ -1,6 +0,0 @@
-extends Label
-
-# Fonction appellé à chaque frame. 'delta' est le temps s'étant écoulé depuis la dernière frame.
-# Elle affiche en information au dessus de celui-ci, la valeure séléctionnée avec le slider. 
-func _process(delta):
-	set_text( "Biais de temperature : " +str($"%tempSlider".value) + "°" )
